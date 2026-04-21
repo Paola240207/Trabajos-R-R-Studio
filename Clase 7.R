82856505f78ddb7d801bb2bad195d8d94f3d3f5f
Paola Constante <paola.constantei@uanl.edu.mx>
2026-04-21 10:07:20 -0600

    esta mal

--- Clase 7.R
+++ Clase 7.R
@@ -3,9 +3,12 @@
 # Datos climatologicos
 # Cumbres NL, Villagran Tamps, Carranza Coahuila
 
+library(lubridate)
 clima <- read.csv("CLIMATOLOGIA.csv", header =T)
 clima$Localidad <- as.factor(clima$Localidad)
 clima$Fecha <- as.Date(clima$Fecha, format = "%d/%m/%Y")
+clima$fechaHora <-as.POSIXct (paste(clima$Fecha, clima$Hora),
+                            format = "%Y-%m-%d %H:%M:%S")
 
 boxplot(clima$TA~ clima$Localidad,
         col= "indianred",
@@ -16,3 +19,33 @@ boxplot(clima$TA~ clima$Localidad,
         outpch = 19) #cambiar la forma a circulo relleno
 
 tapply(clima$TA, clima$Localidad, mean)
+
+# Temperatura en el curso de la semana
+
+Cumbres <- subset(clima,Localidad == "Cumbres")
+Cumbres<- Cumbres[order(Cumbres$Fecha),]
+
+plot(Cumbres$FechaHora, Cumbres$TEMP,
+    type = "l",
+    col = "indianred",
+    lwd =2,
+    xaxt = "n",
+    xlab = "Localidad EMAs",
+    ylab= "Temperatura (°C)")
+
+# Normalidad de datos
+tapply(clima)
+
+
+# ANOVA
+
+clim.aov <- aov(clima$TEMP ~ clima$Localidad)
+summary(clim.aov)
+
+# Aceptamos H1, por lo tanto realizamos una prueba de Tukey
+TukeyHSD(clim.aov)
+
+# Homogeneidad de varianza
+tapply(clima$TEMP, clima$Localidad, var)
+bartlett.test(clima$TEMP ~ clima$Localidad)
+
